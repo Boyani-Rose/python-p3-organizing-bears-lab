780ad48fa8496dfbb68a49868ff0c18d2a6d4d1a
Rose Momanyi <rosebmomanyi@gmail.com>
2025-05-21 14:23:23 +0300

    Completed the lab successfully

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Mr. Chocolate',2, 'M', 'Brown','soft', 1), 
+('Rowdy', 4, 'F', 'White', 'aggressive', 1),
+('Tabitha',5, 'F', 'Brown', 'Friendly', 0),
+('Sergeant Brown',3, 'M', 'Blue', 'Chill', 1),
+('Melissa',7, 'F', 'Black', 'Wise', 1), 
+('Grinch', 5, 'M', 'Green', 'Loud', 1),
+('Wendy', 2, 'F', 'Pink', 'Cool', 1),
+(NULL, 2, 'M', 'White', 'Sleepy', 0);
